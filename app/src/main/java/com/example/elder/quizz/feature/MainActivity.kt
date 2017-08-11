d92c19060962d018ab61845d0e533d684f19adb1
Elder Santos <elder.santos@MESTRE.IEDUCAR>
2017-08-11 17:55:55 -0300

    Ajustes

--- app/src/main/java/com/example/elder/quizz/feature/MainActivity.kt
+++ app/src/main/java/com/example/elder/quizz/feature/MainActivity.kt
@@ -1,44 +1,109 @@
 package com.example.elder.quizz.feature
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import com.example.elder.quizz.R
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
-import model.Questions
+import java.util.*
+import android.os.CountDownTimer
+import android.support.annotation.RequiresApi
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
 
+    val perguntas = arrayListOf<String>()
+    val alternativas = HashMap<Int, List<String>>()
+    val respostas = arrayOf(1,2,2)
+    var numeroPergunta : Int = 0
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get a reference to our posts
-        val database = FirebaseDatabase.getInstance()
-        val ref = database.getReference("alternatives")
-
-        // Attach a listener to read the data at our posts reference
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val post = dataSnapshot.getValue()
-                var text = findViewById<TextView>(R.id.hello)
-                hello.text = post.toString()
-//                System.out.println(post)
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                println("The read failed: " + databaseError.code)
-            }
-        })
+        prepararLista()
+        atualizaPerguntas()
 
     }
 
+    fun prepararLista(){
+
+        perguntas.add("Qual valor de PI?")
+        perguntas.add("Qual o nome do presidente que sofreu iptman em 2016?")
+        perguntas.add("Quem mais roubou em  em 2017")
 
 
+        // adicionando alternativas
+        val primeira = ArrayList<String>()
+        primeira.add("3,14")
+        primeira.add("5")
+        primeira.add("9")
+        primeira.add("5,21")
+
+        val segunda = ArrayList<String>()
+        segunda.add("Michel Temer")
+        segunda.add("Dilma Rulsef")
+        segunda.add("Eneias ")
+        segunda.add("Bolsolixo")
+
+        val terceira = ArrayList<String>()
+        terceira.add("Michel Temer")
+        terceira.add("Michel Temer")
+        terceira.add("Michel Temer")
+        terceira.add("Michel Temer")
+
+        // adicionando a respostas
+        alternativas.put(0 ,primeira )
+        alternativas.put(1 ,segunda)
+        alternativas.put(2 ,terceira)
+
+    }
+
+     fun atualizaPerguntas() {
+
+         if(perguntas.size == numeroPergunta){
+
+             alertaResultado()
+
+         }else{
+             pergunta.text = perguntas[numeroPergunta]
+             alternativa1.text = alternativas[numeroPergunta]?.get(0)
+             alternativa2.text = alternativas[numeroPergunta]?.get(1)
+             alternativa3.text = alternativas[numeroPergunta]?.get(2)
+             alternativa4.text = alternativas[numeroPergunta]?.get(3)
+
+             object : CountDownTimer(30000, 1000) {
+                 override fun onTick(millisUntilFinished: Long) {
+                     textView.text = (millisUntilFinished / 1000).toString()
+
+                 }
+                 override fun onFinish() {
+                     atualizaPerguntas()
+                     textView.setText("done!")
+
+                     atualizaPerguntas()
+                 }
+             }.start()
+
+             numeroPergunta ++
+         }
+     }
+
+    fun alertaResultado() {
+        val alertDialog: AlertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setTitle("Resultado!")
+        alertDialog.setMessage("Voce acertou mizerave")
+        alertDialog.show()
+    }
+    fun checaResposta(view : View){
+
+//        var btn Button = this.getText()
+    }
 }
+
+
---
+++ app/src/main/java/com/example/elder/quizz/feature/Teste.kt
@@ -0,0 +1,18 @@
+package com.example.elder.quizz.feature
+
+import java.util.Arrays
+import java.util.HashMap
+
+/**
+ * Created by elder.santos on 11/08/2017.
+ */
+
+class Teste {
+
+    private fun a() {
+
+        val list = Arrays.asList("3,14", "4", "5")
+        val map = HashMap<String, List<String>>()
+        map.put("Qual valor de PI?", list)
+    }
+}
